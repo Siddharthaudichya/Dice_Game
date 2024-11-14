fee54b7f7d8746ce0e253ca3721c8e0c151b2b42
Siddharth Audichya <siddharthaudichya97@gmail.com>
2024-11-14 10:58:45 +0530

    dice

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -1,32 +1,205 @@
-// let random_number;
-// random_number(Math.random()*6);
-// console.log(random_number);
-
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
-
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
-
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
+//Globals
+//-----For timeouts-----//
+let timeOut1;
+let timeOut2;
+//-----For containers-----//
+let diceNumber1;
+let diceNumber2;
 
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
-
-document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
-
-
-//If player 1 wins
-if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "Player 1 Wins! 🚩";
+//Fixed Re-Roll Spam
+function clearTimeOuts()
+{
+    clearTimeout(timeOut1);
+    clearTimeout(timeOut2);
 }
-else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🔰";
+
+
+//Push names in the cards
+function pushPlayer1()
+{
+    //Get name from input field
+    let player1Name =  $("#player1Input").val().trim();
+
+    //Validate and push names
+    if(player1Name != "")
+    {
+        if(player1Name.length <= 10)
+        {
+            //Plays click sound
+            let click = new Audio("./sounds/click-1.mp3");
+            click.play();
+            $("#player1Name").text(player1Name);
+            $("#player1Input").val("");
+        }
+        else
+        {
+            $("#player1Input").val("");
+        }
+    }
 }
-else {
-  document.querySelector("h1").innerHTML = "Draw 🤐";
+function pushPlayer2()
+{
+    //Get name from input field
+    let player2Name = $("#player2Input").val().trim();
+
+    //Validate and push names
+    if(player2Name != "")
+    {
+        if(player2Name.length <= 10)
+        {
+            //Plays click sound
+            let click = new Audio("./sounds/click-1.mp3");
+            click.play();
+            $("#player2Name").text(player2Name);
+            $("#player2Input").val("");
+        }
+        else
+        {
+            $("#player2Input").val("");
+        }
+    }
 }
+
+
+//Logical Functionm
+function diceGame()
+{
+    clearTimeOuts();
+
+    //Plays dice roll sound
+    let diceRoll = new Audio("./sounds/dice-roll.mp3");
+    diceRoll.play();
+
+    //For Player 1
+    diceNumber1 = Math.floor((Math.random()*6)) + 1;
+    let diceNumber1ImgSrc = "./images/dice" + diceNumber1 + ".gif";
+
+    $(".img1").attr("src", diceNumber1ImgSrc);
+
+    //For Player 2
+    diceNumber2 = Math.floor((Math.random()*6)) + 1;
+    let diceNumber2ImgSrc = "./images/dice" + diceNumber2 + ".gif";
+
+    $(".img2").attr("src", diceNumber2ImgSrc);
+
+
+    //Choose Winner Function
+    function chooseWinner()
+    {
+        //If Player 1 Wins
+        if(diceNumber1 > diceNumber2)
+        {
+            let winner = document.getElementById("player1Name").textContent;
+            declareWinner("🚩"+winner+" wins!");
+        }
+        //If Draw
+        else if (diceNumber1 == diceNumber2)
+        {
+            declareWinner("Draw");
+        }
+        //If Player 2 Wins
+        else
+        {
+            let winner = document.getElementById("player2Name").textContent;
+            declareWinner(winner+" wins!🚩");
+        }
+    }
+    timeOut1 = setTimeout(chooseWinner, 1000);
+
+
+    //Declare Winner Function
+    function declareWinner(whoWins)
+    {
+        $("#win").text(whoWins);
+    }
+
+
+    $(".btn:first").text("Re-Roll");
+
+
+    //Animation Function
+    $("#win").css("opacity", "0");
+    function winAnimation()
+    {
+        document.getElementById("win").style.transitionDuration = "200ms";
+        document.getElementById("win").style.opacity = "1";
+    }
+    timeOut2 = setTimeout(winAnimation, 3000);
+}
+
+
+//Animation Function For Input Label 'Name'
+function setTransitionDelay1()
+{
+        $(".player-1").css({"transition-delay":"400ms"});
+        setTimeout(function() {$(".player-1").css({"transition-delay":"0ms"});}, 300);
+}
+function setTransitionDelay2()
+{
+        $(".player-2").css({"transition-delay":"400ms"});
+        setTimeout(function() {$(".player-2").css({"transition-delay":"0ms"});}, 300);
+}
+
+
+//Resets Everything
+function reset()
+{
+    //Clear all timeouts
+    clearTimeOuts();
+    //Resets containers
+    diceNumber1 = 0;
+    diceNumber2 = 0;
+
+    //Resets Player names in the card
+    $("#player1Name").text("Player 1");
+    $("#player2Name").text("Player 2");
+    //Reverts back the css for "#win"
+    $("#win").css({"opacity":"0", "transition-duration":"200ms"});
+    //Chnages Roll button text to "Roll"
+    $(".btn:first").text("Roll");
+    //Resets the gif to Dice 6 png
+    $(".img1").attr("src", "./images/dice6.png");
+    $(".img2").attr("src", "./images/dice6.png");
+}
+
+
+//Event Listener
+
+//For Player 1 Name Input
+$("#player1Input").keydown(handleButtonForName1);
+function handleButtonForName1(event)
+{
+    //Push names on Enter
+    if(event.key == "Enter")
+    {
+        pushPlayer1();
+    }
+}
+//For Player 2 Name Input
+$("#player2Input").keydown(handleButtonForName2);
+function handleButtonForName2(event)
+{
+    //Push names on Enter
+    if(event.key == "Enter")
+    {
+        pushPlayer2();
+    }
+}
+
+
+// Preloading audio files
+var audioFiles = [
+    "./sounds/click-1.mp3",
+    "./sounds/dice-roll.mp3"
+];
+
+function preloadAudio() {
+    for (var i = 0; i < audioFiles.length; i++) {
+        var audio = new Audio();
+        audio.src = audioFiles[i];
+        audio.preload = "auto";
+    }
+}
+// Call the preloadAudio function when the window loads
+window.onload = preloadAudio;
